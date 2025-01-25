832a6742da01517cba784bc9b96a8c06418a5f8e
Sudip Ghimire <ghimiresdp@gmail.com>
2025-01-25 21:16:24 +0545

    add retrieve API

--- projects/todo-web/src/api.rs
+++ projects/todo-web/src/api.rs
@@ -1,6 +1,6 @@
 use actix_web::{web, HttpResponse, Responder};
 
-use crate::{todo::TodoList, ApplicationState};
+use crate::ApplicationState;
 
 pub(crate) async fn list(data: web::Data<ApplicationState>) -> impl Responder {
     let items = (&data.todo_list.lock().unwrap().items).clone();
@@ -12,9 +12,14 @@ pub(crate) async fn create(data: web::Data<ApplicationState>) -> impl Responder 
     let todo_list = data.todo_list.lock().unwrap();
     HttpResponse::Ok().body("[]")
 }
-pub(crate) async fn retrieve(data: web::Data<ApplicationState>) -> impl Responder {
-    let todo_list = data.todo_list.lock().unwrap();
-    HttpResponse::Ok().body("[]")
+pub(crate) async fn retrieve(
+    data: web::Data<ApplicationState>,
+    path: web::Path<(usize,)>,
+) -> impl Responder {
+    let items = &data.todo_list.lock().unwrap().items;
+    let item = (items).get(&path.into_inner().0).unwrap();
+    let str = serde_json::to_string(item).unwrap();
+    HttpResponse::Ok().body(str)
 }
 pub(crate) async fn update(data: web::Data<ApplicationState>) -> impl Responder {
     let todo_list = data.todo_list.lock().unwrap();
