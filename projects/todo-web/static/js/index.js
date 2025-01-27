5c544c0b2d1fce739c562b3f9cb8d0c5c8bce8b2
Sudip Ghimire <ghimiresdp@gmail.com>
2025-01-27 23:51:19 +0545

    feat: update UI to use tailwind css from cdn and add list and create actions in UI.

--- projects/todo-web/static/js/index.js
+++ projects/todo-web/static/js/index.js
@@ -7,6 +7,9 @@ const todo = {
             console.log("Failed to fetch")
             return [];
         }
+    },
+    create: async () => {
+        await fetch("/api/todo/", {method: 'POST', body})
     }
 }
 
@@ -15,7 +18,32 @@ const todo = {
 document.addEventListener('DOMContentLoaded', async () => {
     const sidebar = document.querySelector('div.sidebar');
     const elements = (await todo.list()).map(d => {
-        return `<div class="todo${d.checked?' checked':''}">${d.title}</div>`
+        return `<div class="todo${d.checked ? ' checked' : ''}">${d.title}</div>`
     });
     sidebar.innerHTML = elements.join('');
-})
+
+    // add new todo event listener
+    document.querySelector('button#add-todo').addEventListener('click', () => {
+        document.querySelector("#add-modal").classList.toggle('hidden');
+    })
+
+    document.querySelector("#submit-todo").addEventListener('click', (event) => {
+        event.preventDefault();
+        const title = document.querySelector("input#title").value;
+        const content = document.querySelector("textarea#content").value;
+        fetch("/api/todo/", {
+            method: 'POST',
+            body: {
+                title,
+                content,
+            },
+        })
+    });
+
+    document.querySelector("#cancel-todo").addEventListener('click', (event) => {
+        document.querySelector("input#title").value = "";
+        document.querySelector("textarea#content").value="";
+        document.querySelector("#add-modal").classList.add('hidden');
+    });
+
+});
