7c1c50448546dafdd0dae0c723eb9486b0a31558
Sudip Ghimire <ghimiresdp@gmail.com>
2025-02-15 16:29:32 +0545

    feat: more trait example with bounds

--- advanced/types-and-generics/traits.rs
+++ advanced/types-and-generics/traits.rs
@@ -1,5 +1,5 @@
 /// !to run, execute `cargo run --bin traits`
-use std::f64::consts::PI;
+use std::{f64::consts::PI, fmt::Debug};
 
 /// #Traits
 /// -------
@@ -17,52 +17,93 @@ use std::f64::consts::PI;
 /// the boundary of OOPs in rust by working in an object-oriented approach.
 
 fn main() {
+    // =========================================================================
+    // The below example uses a trait `Shape` and gets implemented in different
+    // structures `Rect` and `Circle`.
     println!("Traits");
-
-    // using traits in different shapes
-    {
-        struct Rect {
-            w: f64,
-            h: f64,
-        }
-
-        struct Circle {
-            r: f64,
-        }
-
-        // define trait Shape
-        trait Shape {
-            fn area(&self) -> f64;
-            fn perimeter(&self) -> f64;
-        }
-
-        // implement trait on different structs `Rect` and `Circle`
-        impl Shape for Rect {
-            fn area(&self) -> f64 {
-                self.w * self.h
-            }
-
-            fn perimeter(&self) -> f64 {
-                2f64 * (self.w + self.h)
-            }
-        }
-
-        impl Shape for Circle {
-            fn area(&self) -> f64 {
-                PI * self.r.powi(2)
-            }
-
-            fn perimeter(&self) -> f64 {
-                2f64 * PI * self.r
-            }
-        }
-
-        let rectangle = Rect { w: 5f64, h: 10f64 };
-        let circle = Circle { r: 10f64 };
-
-        println!("Area of rectangle is: {}", rectangle.area());
-        println!("Perimeter of rectangle is: {}", rectangle.perimeter());
-        println!("Area of circle is: {}", circle.area());
-        println!("Perimeter of circle is: {}", rectangle.perimeter());
+    #[derive(Debug)]
+    struct Rect {
+        w: f64,
+        h: f64,
     }
+    #[derive(Debug)]
+    struct Circle {
+        r: f64,
+    }
+
+    // define trait Shape
+    trait Shape {
+        fn area(&self) -> f64;
+        fn perimeter(&self) -> f64;
+    }
+
+    // =========================================================================
+    // implement trait on different structs `Rect` and `Circle`
+    impl Shape for Rect {
+        fn area(&self) -> f64 {
+            self.w * self.h
+        }
+
+        fn perimeter(&self) -> f64 {
+            2f64 * (self.w + self.h)
+        }
+    }
+
+    impl Shape for Circle {
+        fn area(&self) -> f64 {
+            PI * self.r.powi(2)
+        }
+
+        fn perimeter(&self) -> f64 {
+            2f64 * PI * self.r
+        }
+    }
+
+    let rectangle = Rect { w: 5f64, h: 10f64 };
+    let circle = Circle { r: 10f64 };
+
+    println!("Area of rectangle is: {}", rectangle.area());
+    println!("Perimeter of rectangle is: {}", rectangle.perimeter());
+    println!("Area of circle is: {}", circle.area());
+    println!("Perimeter of circle is: {}", rectangle.perimeter());
+
+    // =========================================================================
+    // Not only we can implement trait on a structure, we can also pass a
+    // parameter as a trait. This can also be thought of a generic parameter
+    // since, we can pass different type of object as a parameter that has
+    // implemented that specific trait.
+
+    fn describe_area(shape: &(impl Shape + Debug)) {
+        println!("The area of {:?} is: {}", shape, shape.area());
+    }
+
+    describe_area(&rectangle);
+    describe_area(&circle);
+
+    // =========================================================================
+    // Instead of passing trait as a parameter, it will be easier to read and
+    // the code will be clean enough if we implement it using the trait bound
+    // syntax.
+    //
+    // Use trait as a boundary or trait bound
+
+    fn describe_perimeter<T: Shape + Debug>(shape: &T) {
+        println!("The area of {:?} is: {}", shape, shape.perimeter())
+    }
+    describe_perimeter(&rectangle);
+    describe_perimeter(&circle);
+
+    // =========================================================================
+    // we can even implement trait bound using `where` clause so that code will
+    // be more readable.
+    fn describe_area_1<T>(shape: &T)
+    where
+        T: Debug + Shape,
+    {
+        println!("The area of {:?} using `where` is: {}", shape, shape.area());
+    }
+    describe_area_1(&rectangle);
+    describe_area_1(&circle);
+
+    // =========================================================================
 }
