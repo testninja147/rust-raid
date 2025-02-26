61b782b94f9725bec3d8683a452077fd51bf2b4f
Sudip Ghimire <ghimiresdp@gmail.com>
2025-02-27 00:41:20 +0545

    feat: add derive macro documentation

--- advanced/meta-programming/src/derive-macro.rs
+++ advanced/meta-programming/src/derive-macro.rs
@@ -1,24 +1,56 @@
+// ! to run, execute: cargo run --bin derive
 use xml_derive::Serialize;
 
-#[allow(dead_code)]
 #[derive(Debug, Serialize)]
 struct Student {
     id: usize,
     first_name: String,
     last_name: String,
-    // subjects: Vec<String>,
+    major: String,
 }
+
+/// # Derive Macro
+/// ---------------
+///
+/// Derive macro is a procedural macro, that generates code from the given token
+/// stream. It adds a new input interface to the #[Derive] attribute.
+///
+/// Some of the builtin procedural macros are as follows:
+/// - Debug
+/// - Display
+/// - Copy
+/// - Clone
+///
+/// We use derive macro to add complex functionality, for example serializing
+/// and deserializing our data. In the example below, we have implemented a
+/// xml serialization method using the custom derive macro.
+///
+///
+/// To create a derive macro, we have to create a new library create that needs
+/// to have a cargo.toml configured to add the following property:
+///
+/// ```toml
+/// ...
+///
+/// [lib]
+/// proc-macro = true
+/// ...
+///
+/// ```
+///
+/// After creating the lib, we need to import the library to our implementation
+/// part.
+///
+/// To know more about procedural macro, please check the following:
+/// https://doc.rust-lang.org/reference/procedural-macros.html#procedural-macros
 fn main() {
     println!("Derive Macro");
     let student_1 = Student {
         id: 1,
         first_name: String::from("John"),
         last_name: String::from("Doe"),
-        // subjects: vec![
-        //     String::from("Computer Science"),
-        //     String::from("Mathematics"),
-        // ],
+        major: String::from("Computer Science"),
     };
     println!("Student 1: {:?}", student_1);
-    println!("Serialized Data:\n{}", student_1.serialize());
+    println!("Serialized Data:\n{}", student_1.to_xml());
 }
--- advanced/meta-programming/xml-derive/src/lib.rs
+++ advanced/meta-programming/xml-derive/src/lib.rs
@@ -2,6 +2,15 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{DeriveInput, parse_macro_input};
 
+/// # Procedural Macro
+/// -------------------
+///
+/// A procedural macro is another macro type, that accepts token stream and then
+/// adds additional property to the existing data and generates code at compile
+/// time.
+///
+/// To know more about procedural macro, please check the following:
+/// https://doc.rust-lang.org/reference/procedural-macros.html#procedural-macros
 #[proc_macro_derive(Serialize)]
 pub fn serialize(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -14,7 +23,7 @@ pub fn serialize(input: TokenStream) -> TokenStream {
     };
 
     // Collect the names of the struct's fields
-    let tags = fields.iter().map(|f| {
+    let field_name = fields.iter().map(|f| {
         let k = f.ident.as_ref().unwrap(); // unwrap here as we expect named fields
         quote! { #k }
     });
@@ -22,11 +31,11 @@ pub fn serialize(input: TokenStream) -> TokenStream {
     let expanded = quote! {
 
         impl #name {
-            fn serialize(&self)-> String{
+            fn to_xml(&self)-> String{
                 let children = vec![
-                    #(format!("<{k}>{v}</{k}>", k=stringify!(#tags).to_string(), v=self.#tags),)*
+                    #(format!("<{k}>{v}</{k}>", k=stringify!(#field_name).to_string(), v=self.#field_name),)*
                 ];
-                return format!("<{k}>{v}</{k}>",k=stringify!(#name),v=children.join(""));
+                return format!("<{k}>\n  {v}\n</{k}>",k=stringify!(#name),v=children.join("\n  "));
             }
         }
     };
