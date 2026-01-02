ca5e47fd0e399cb62b2f06d88a3c2960a3bf52ec
Sudip <ghimiresdp@gmail.com>
2026-01-02 06:05:40 +0000

    feat: update documentation

--- algorithms/src/misc/luhn.rs
+++ algorithms/src/misc/luhn.rs
@@ -5,26 +5,33 @@
 //!
 //! Example: the following algorithm describes validating credit card number.
 //!
-//! ```text
-//! function isValid(cardNumber[1..length])
-//!     sum := 0
-//!     parity := length mod 2
-//!     for i from 1 to (length - 1) do
-//!         if i mod 2 == parity then
-//!             sum := sum + cardNumber[i]
-//!         elseif cardNumber[i] > 4 then
-//!             sum := sum + 2 * cardNumber[i] - 9
-//!         else
-//!             sum := sum + 2 * cardNumber[i]
-//!         end if
-//!     end for
-//!     return cardNumber[length] == ((10 - (sum mod 10)) mod 10)
-//! end function
-//! ```
+//! # Algorithm
+//!
+//! 1. remove spaces from string if it contains any.
+//!
+//!    `4539 3195 0343 6467` -> `4539319503436467`
+//!
+//! 2. for every second digit (example 1st, 3rd, ...,) double the number.
+//!
+//!    `(4*2=8) 5 (3*2=6) 9  (3*2=6) 1 (9*2=18) 5 (0*2=0) 3 (4*2=8) 3 (6*2=12) 4 (6*2=12) 7`
+//!
+//! 3. if the doubled number is greater than 9, subtract it by 9 to get single digit.
+//!
+//!    `(8) 5 (6) 9  (6) 1 (18-9=9) 5 (0) 3 (8) 3 (12-9=3) 4 (12-9=3) 7`
+//!
+//!    = `(8) 5 (6) 9  (6) 1 (9) 5 (0) 3 (8) 3 (3) 4 (3) 7`
+//!
+//! 4. add all digits
+//!
+//!    `8 + 5 + 6 + 9 + 6 + 1 + 9 + 5 + 0 + 3 + 8 + 3 + 3 + 4 + 3 + 7`
+//!    = `80`
+//!
+//! 5. if it is divisible by 10, it is valid card number else not.
+//!    Here 80 %10 = 0 so it is a valid card number.
 //!
 //! for more details, check <https://en.wikipedia.org/wiki/Luhn_algorithm>.
 
-use core::num;
+use common::input;
 
 struct CreditCard {
     number: String,
@@ -34,15 +41,16 @@ impl CreditCard {
         return Self { number };
     }
     fn is_valid(&self) -> bool {
-        println!("Input: {:?}", &self.number);
         let number = self
             .number
-            .replace(" ", "")
+            .replace(" ", "") // remove spaces if available
             .chars()
-            .map(|c| c.to_digit(10).unwrap())
+            .map(|c| c.to_digit(10).unwrap()) // parse as u32
             .enumerate()
             .map(|(idx, num)| {
+                // check alternating digits for validation
                 if idx % 2 == 0 {
+                    // if the double of the digit is greater than 9, subtract it by 9
                     if num * 2 < 9 { num * 2 } else { num * 2 - 9 }
                 } else {
                     num
@@ -58,6 +66,12 @@ impl CreditCard {
 }
 
 fn main() {
-    let card = CreditCard::new("4539 3195 0343 6467".to_owned());
-    println!("valid: {}", card.is_valid())
+    // println!("valid: {}", card.is_valid());
+    let card_number =
+        input("Enter card number (with or without spaces)\n[example: 4539 3195 0343 6467]: ");
+    let card = CreditCard::new(card_number);
+    println!(
+        "The card number is {}.",
+        if card.is_valid() { "valid" } else { " invalid" }
+    )
 }
